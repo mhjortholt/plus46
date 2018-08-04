e3b20eb3dc77673b7999c9a9303e5a47bb8a45aa
Micke <michael.hjortholt@gmail.com>
2018-08-04 11:38:52 +0200

    added keep server alive ping

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const https = require('https')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +9,13 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+// Keep server awake
+setInterval(function() {
+	https.get('https://plus46.herokuapp.com', (resp) => {
+	  console.log('Ping!');
+	}).on("error", (err) => {
+	  console.log("Error when pinging: " + err.message);
+	});
+}, 13*60*1000);
